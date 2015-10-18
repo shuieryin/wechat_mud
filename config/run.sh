a3690ed15839111c6bc429e6f584cd6ddcac9085
shuieryin <shuieryin@gmail.com>
2015-10-18 21:40:08 +0000

    relflow test

--- config/run.sh
+++ config/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /root/workspaces/wechat_mud/_build/default/rel/wechat_mud/
+cd /root/workspaces/wechat_mud/_build/default/rel/wechat_mud_release/
 redis-server &
-./bin/wechat_mud console
+./bin/wechat_mud_release console
