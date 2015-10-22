d6ee752736dcad337322a138a136a6fc9a6ec721
CollinGuo <shuieryin@gmail.com>
2015-10-22 09:40:36 +0800

    generate appup script initial

---
+++ config/generate_appup.erl
@@ -0,0 +1,23 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -smp enable -sname generate_appup verbose
+
+main([]) ->
+    try
+        Output = os:cmd("rebar3 path --app wechat_mud"),
+        io:format("~p~n", [Output])
+    catch
+        _:_ ->
+            usage()
+    end;
+main(_) ->
+    usage().
+
+usage() ->
+    io:format("usage: version x.x.x\n"),
+    halt(1).
+
+generate_appups(Map, State) when is_map(Map) ->
+    maps:map(fun(K,V) ->
+        create_appup_term(generate_appup(K,V,State))
+    end, Map).
