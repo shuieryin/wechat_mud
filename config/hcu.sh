00873344b261b453f25d67f644e3dee5a6f63921
CollinGuo <shuieryin@gmail.com>
2015-10-24 22:43:05 +0800

    generate appup script done

--- config/hcu.sh
+++ config/hcu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-rebar3 compile
-CURRENT_TAG=$(git describe --abbrev=0 --tags)
-CURRENT_VERSION=$(echo ${CURRENT_TAG} | sed -e "s/v//g")
+APP_NAME=$(cat rebar.config | grep app-name-marker | awk '{print $1}' | tr -d ,)
+OLD_VSN=$(cat rebar.config | grep release-version-marker | awk '{print $1}' | tr -d \")
 
-git commit -a -m upgrade_from_${CURRENT_TAG}
+NEW_VSN=$(./config/appup ${APP_NAME} ${OLD_VSN} | tr -d \")
 
-rebar3 relflow -u ${CURRENT_TAG} -x $(./config/incre_ver ${CURRENT_VERSION} | sed s/\"//g)
-rebar3 release relup -u ${CURRENT_VERSION} tar
+if [ "${NEW_VSN}" = "no_change" ]; then
+    echo "No changed files"
+else
+    rebar3 release relup -u ${OLD_VSN} tar
+    yes | cp -i _build/default/rel/${APP_NAME}/releases/${NEW_VSN}/${APP_NAME}.rel _build/default/rel/${APP_NAME}/releases/
 
-LATEST_VERSION=$(git describe --abbrev=0 --tags | sed -e "s/v//g")
-
-mv _build/default/rel/wechat_mud/wechat_mud-${LATEST_VERSION}.tar.gz _build/default/rel/wechat_mud/releases/${LATEST_VERSION}/wechat_mud.tar.gz
-./_build/default/rel/wechat_mud/bin/wechat_mud install ${LATEST_VERSION}
+    mv _build/default/rel/${APP_NAME}/${APP_NAME}-${NEW_VSN}.tar.gz _build/default/rel/${APP_NAME}/releases/${NEW_VSN}/wechat_mud.tar.gz
+    ./_build/default/rel/${APP_NAME}/bin/${APP_NAME} install ${NEW_VSN}
+fi
--- config/run.sh
+++ config/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /root/workspaces/wechat_mud/_build/default/rel/wechat_mud/
+cd _build/default/rel/wechat_mud/
 redis-server &
 ./bin/wechat_mud console
