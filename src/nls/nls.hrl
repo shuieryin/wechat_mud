9d60f7ae494d40b08e2aa4ac10f5fd532096e73d
CollinGuo <shuieryin@gmail.com>
2015-10-29 23:13:09 +0800

    comments for nls header

--- src/nls/nls.hrl
+++ src/nls/nls.hrl
@@ -1,13 +1,20 @@
 %%%-------------------------------------------------------------------
 %%% @author shuieryin
-%%% @copyright (C) 2015, <COMPANY>
+%%% @copyright (C) 2015, Shuieryin
 %%% @doc
+%%%
+%%% Header file for nls_server.
 %%%
 %%% @end
 %%% Created : 16. Aug 2015 8:32 PM
 %%%-------------------------------------------------------------------
 -author("shuieryin").
 
+%% Nls files root path
 -define(NLS_PATH, "runtime_files/nls").
+
+%% Common nls file name
 -define(COMMON_NLS, "common.csv").
+
+%% Unique nls file extension
 -define(NLS_EXTENSION, ".csv").
