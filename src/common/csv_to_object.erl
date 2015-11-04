bd13dcf5046d4f0311d6e1a17fb1f4c26b8e7e3b
CollinGuo <shuieryin@gmail.com>
2015-11-04 21:25:09 +0800

    extract parsing csv file functions to csv_to_object module

---
+++ src/common/csv_to_object.erl
@@ -0,0 +1,180 @@
+%%%-------------------------------------------------------------------
+%%% @author shuieryin
+%%% @copyright (C) 2015, Shuieryin
+%%% @doc
+%%%
+%%% Converts csv files to object configs according to function
+%%% provided from caller.
+%%%
+%%% @end
+%%% Created : 04. Nov 2015 9:13 PM
+%%%-------------------------------------------------------------------
+-module(csv_to_object).
+-author("shuieryin").
+
+%% API
+-export([traverse_files/2,
+    parse_file/2]).
+
+-define(FILE_EXTENSION, ".csv").
+-define(NAME_TYPE_SEPARATOR, ":").
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates object configs from files of given folder.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec traverse_files(FolderPath, RowFun) -> ListFromFiles when
+    FolderPath :: file:name(),
+    RowFun :: function(),
+    ListFromFiles :: [term()].
+traverse_files(FolderPath, RowFun) ->
+    {ok, FileNameList} = file:list_dir(FolderPath),
+    traverse_files(FileNameList, [], RowFun, FolderPath).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates object configs from file which the number of object
+%% configs equals to the number of rows.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec parse_file(FilePath, RowFun) -> ListFromFile when
+    FilePath :: file:filename(),
+    RowFun :: function,
+    ListFromFile :: [term()].
+parse_file(FilePath, RowFun) ->
+    {ok, File} = file:open(FilePath, [read]),
+    {ok, ListFromFile} = ecsv:process_csv_file_with(File, fun traverse_rows/2, {0, RowFun}),
+    ok = file:close(File),
+    ListFromFile.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates object configs per file of csv file under given folder
+%% which the number of object configs equals to the number of rows
+%% times number of csv files.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec traverse_files(FileNameList, AccFileList, RowFun, FolderPath) -> ListFromFiles when
+    FileNameList :: filename:filename(),
+    AccFileList :: [term()],
+    RowFun :: function(),
+    FolderPath :: file:name(),
+    ListFromFiles :: [term()].
+traverse_files([], AccFileList, _, _) ->
+    AccFileList;
+traverse_files([FileName | Tail], AccFileList, RowFun, FolderPath) ->
+    case filename:extension(FileName) == ?FILE_EXTENSION of
+        true ->
+            CurListFromFile = parse_file(filename:join(FolderPath, FileName), RowFun),
+            traverse_files(Tail, CurListFromFile ++ AccFileList, RowFun, FolderPath);
+        _ ->
+            traverse_files(Tail, AccFileList, RowFun, FolderPath)
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Reads line from csv file
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec traverse_rows(NewRowData, State) -> RowValueList when
+    NewRowData :: {newline, NewRow} | {eof},
+    NewRow :: [term()],
+    RowFun :: function(),
+    State :: {Counter, RowFun, KeysMap, AccRowValueList} | {0, RowFun} | {_, _, _, RowValueList},
+    Counter :: non_neg_integer(),
+    KeysMap :: map(),
+    AccRowValueList :: [term()],
+    RowValueList :: [term()].
+traverse_rows({newline, NewRow}, {Counter, RowFun, KeysMap, AccRowValueList}) ->
+    CurRowValuesMap = traverse_column(NewRow, KeysMap, #{}, 0),
+    CurRowValue = RowFun(CurRowValuesMap),
+    {Counter + 1, RowFun, KeysMap, [CurRowValue | AccRowValueList]};
+traverse_rows({newline, NewRow}, {0, RowFun}) ->
+    KeysMap = gen_keysmap(NewRow, #{}, 0),
+    {1, RowFun, KeysMap, []};
+traverse_rows({eof}, {_, _, _, RowValueList}) ->
+    RowValueList.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates keys map from first row
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec gen_keysmap(NewRow, KeysMap, Pos) -> FinalKeysMap when
+    NewRow :: [term()],
+    KeysMap :: map(),
+    Pos :: non_neg_integer(),
+    FinalKeysMap :: map().
+gen_keysmap([], KeysMap, _) ->
+    KeysMap;
+gen_keysmap([RawKeyStr | Tail], KeysMap, Pos) ->
+    gen_keysmap(Tail, KeysMap#{Pos => RawKeyStr}, Pos + 1).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Generates values map from rest of columns of current row.
+%%
+%% Supported types:
+%% integer, float, atom, string, and other terms
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec traverse_column(RowData, KeysMap, ValuesMap, Pos) -> FinalValuesMap when
+    RowData :: [term()],
+    KeysMap :: map(),
+    ValuesMap :: map(),
+    Pos :: non_neg_integer(),
+    FinalValuesMap :: map().
+traverse_column([], _, ValuesMap, _) ->
+    ValuesMap;
+traverse_column([[] | Tail], KeysMap, ValuesMap, Pos) ->
+    traverse_column(Tail, KeysMap, ValuesMap, Pos + 1);
+traverse_column([RawValue | TailValues], KeysMap, ValuesMap, Pos) ->
+    FieldNameInfoStr = maps:get(Pos, KeysMap),
+    [FieldNameStr | TailFieldTypeStr] = string:tokens(FieldNameInfoStr, ?NAME_TYPE_SEPARATOR),
+    FieldName = list_to_atom(FieldNameStr),
+    FieldType =
+        case TailFieldTypeStr of
+            [] ->
+                string;
+            [FieldTypeStr | _] ->
+                list_to_atom(FieldTypeStr)
+        end,
+    Value =
+        case FieldType of
+            atom ->
+                list_to_atom(RawValue);
+            integer ->
+                list_to_integer(RawValue);
+            float ->
+                list_to_float(RawValue);
+            term ->
+                {ok, Tokens, _} = erl_scan:string(RawValue),
+                {ok, Term} = erl_parse:parse_term(Tokens),
+                Term;
+            _ ->
+                RawValue
+        end,
+    UpdatedValuesMap =
+        case Value of
+            undefined ->
+                ValuesMap;
+            _ ->
+                ValuesMap#{FieldName => Value}
+        end,
+    traverse_column(TailValues, KeysMap, UpdatedValuesMap, Pos + 1).
--- src/scene/scene_sup.erl
+++ src/scene/scene_sup.erl
@@ -24,6 +24,8 @@
 -define(SERVER, ?MODULE).
 -include("scene.hrl").
 
+-type scene_child() :: {atom(), {scene_fsm, start_link, [{init, map()}]}, supervisor:restart(), supervisor:shutdown(), supervisor:worker(), [scene_fsm]}.
+
 %%%===================================================================
 %%% API functions
 %%%===================================================================
@@ -62,32 +64,12 @@ init([]) ->
 
     SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
 
-    ChildList = gen_child_list(),
+    ChildList = gen_scene_child_list(),
     {ok, {SupFlags, ChildList}}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Generates scene worker configs entry.
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec gen_child_list() -> ChildList when
-    ChildList :: [{WorkerName, {scene_fsm, start_link, []}, Restart, Shutdown, Type, [scene_fsm]}],
-    Restart :: supervisor:restart(),
-    Shutdown :: supervisor:shutdown(),
-    Type :: supervisor:worker(),
-    WorkerName :: atom().
-gen_child_list() ->
-    Restart = permanent,
-    Shutdown = 2000,
-    Type = worker,
-
-    {ok, FileNameList} = file:list_dir(?SCENE_PATH),
-    gen_child_list(FileNameList, [], Restart, Shutdown, Type).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -97,113 +79,30 @@ gen_child_list() ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec gen_child_list(FileNameList, AccSceneList, Restart, Shutdown, Type) -> [term()] when
-    FileNameList :: filename:filename(),
-    AccSceneList :: [term()],
-    Restart :: atom(),
-    Shutdown :: pos_integer(),
-    Type :: atom().
-gen_child_list([], AccSceneList, _, _, _) ->
-    AccSceneList;
-gen_child_list([FileName | Tail], AccSceneList, Restart, Shutdown, Type) ->
-    case filename:extension(FileName) == ?FILE_EXTENSION of
-        true ->
-            {ok, SceneFile} = file:open(filename:join(?SCENE_PATH, FileName), [read]),
-            {ok, CurScenesList} = ecsv:process_csv_file_with(SceneFile, fun gen_scenes/2, {0, Restart, Shutdown, Type}),
-            ok = file:close(SceneFile),
+-spec gen_scene_child_list() -> SceneChildList when
+    SceneChildList :: [scene_child()].
+gen_scene_child_list() ->
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
 
-            gen_child_list(Tail, CurScenesList ++ AccSceneList, Restart, Shutdown, Type);
-        _ ->
-            gen_child_list(Tail, AccSceneList, Restart, Shutdown, Type)
-    end.
-%%--------------------------------------------------------------------
-%% @doc
-%% Reads line from csv file
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec gen_scenes(NewLineData, State) -> FinalSceneList when
-    NewLineData :: {newline, NewLine} | {eof},
-    NewLine :: [term()],
-    State :: {Counter, Restart, Shutdown, Type, KeysMap, SceneList} | {0, Restart, Shutdown, Type} | {_, _, _, _, _, FinalSceneList},
-    Counter :: non_neg_integer(),
-    Restart :: atom(),
-    Shutdown :: pos_integer(),
-    Type :: atom(),
-    KeysMap :: map(),
-    SceneList :: [term()],
-    FinalSceneList :: [term()].
-gen_scenes({newline, NewLine}, {Counter, Restart, Shutdown, Type, KeysMap, SceneList}) ->
-    CurScene = gen_scene(NewLine, KeysMap, #{}, 0, Restart, Shutdown, Type),
-    {Counter + 1, Restart, Shutdown, Type, KeysMap, [CurScene | SceneList]};
-gen_scenes({newline, NewLine}, {0, Restart, Shutdown, Type}) ->
-    KeysMap = gen_keysmap(NewLine, #{}, 0),
-    {1, Restart, Shutdown, Type, KeysMap, []};
-gen_scenes({eof}, {_, _, _, _, _, FinalSceneList}) ->
-    FinalSceneList.
+    ChildFun = fun(SceneValuesMap) ->
+        populate_scene_child(SceneValuesMap, Restart, Shutdown, Type)
+    end,
+
+    csv_to_object:traverse_files(?SCENE_PATH, ChildFun).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Generates keys map from first line
+%% Generates scene worker configs entry.
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec gen_keysmap(NewLine, KeysMap, Pos) -> FinalKeysMap when
-    NewLine :: [term()],
-    KeysMap :: map(),
-    Pos :: non_neg_integer(),
-    FinalKeysMap :: map().
-gen_keysmap([], KeysMap, _) ->
-    KeysMap;
-gen_keysmap([RawKey | Tail], KeysMap, Pos) ->
-    Key = list_to_atom(RawKey),
-    gen_keysmap(Tail, KeysMap#{Pos => Key}, Pos + 1).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Generates values map from rest of lines
-%% todo: the scene csv file is so far hard coded under this function, will need to find a way to dynamically populates values from each row.
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec gen_scene(NewLine, KeysMap, ValuesMap, Pos, Restart, Shutdown, Type) -> SceneChild when
-    NewLine :: [term()],
-    KeysMap :: map(),
-    ValuesMap :: map(),
-    Pos :: non_neg_integer(),
-    Restart :: atom(),
-    Shutdown :: pos_integer(),
-    Type :: atom(),
-    Id :: atom(),
-    SceneChild :: {Id, {scene_fsm, start_link, [{init, ValuesMap}]}, Restart, Shutdown, Type, [scene_fsm]}.
-gen_scene([], _, #{id := Id} = ValuesMap, _, Restart, Shutdown, Type) ->
-    {Id, {scene_fsm, start_link, [{init, ValuesMap}]}, Restart, Shutdown, Type, [scene_fsm]};
-gen_scene([[] | Tail], KeysMap, ValuesMap, Pos, Restart, Shutdown, Type) ->
-    gen_scene(Tail, KeysMap, ValuesMap, Pos + 1, Restart, Shutdown, Type);
-gen_scene([RawValue | Tail], KeysMap, ValuesMap, Pos, Restart, Shutdown, Type) ->
-    Key = maps:get(Pos, KeysMap),
-    Value =
-        case Key of
-            id ->
-                list_to_atom(RawValue);
-            exits ->
-                {ok, Tokens, _} = erl_scan:string(RawValue),
-                {ok, Term} = erl_parse:parse_term(Tokens),
-                Term;
-            title ->
-                list_to_atom(RawValue);
-            desc ->
-                list_to_atom(RawValue);
-            nls_server ->
-                list_to_atom(RawValue);
-            _ ->
-                undefined
-        end,
-    UpdatedValuesMap =
-        case Value of
-            undefined ->
-                ValuesMap;
-            _ ->
-                ValuesMap#{Key => Value}
-        end,
-    gen_scene(Tail, KeysMap, UpdatedValuesMap, Pos + 1, Restart, Shutdown, Type).
+-spec populate_scene_child(SceneValuesMap, Restart, Shutdown, Type) -> SceneChild when
+    SceneValuesMap :: map(),
+    Restart :: supervisor:restart(),
+    Shutdown :: supervisor:shutdown(),
+    Type :: supervisor:worker(),
+    SceneChild :: scene_child().
+populate_scene_child(#{id := Id} = SceneValuesMap, Restart, Shutdown, Type) ->
+    {Id, {scene_fsm, start_link, [{init, SceneValuesMap}]}, Restart, Shutdown, Type, [scene_fsm]}.
