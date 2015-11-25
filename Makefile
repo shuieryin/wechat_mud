3cc083ccc8698400d8fa30856d6e895cd68c3526
CollinGuo <shuieryin@gmail.com>
2015-11-25 21:48:42 +0800

    change status border to flat square

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-all:
+all: install
+
+install:
 	@./config/install.sh
 
 cu: clean_upgrade
@@ -18,3 +20,8 @@ run:
 reset:
 	git fetch --all
 	git reset --hard origin/master
+
+ct:
+	#@cd _build/default/rel/wechat_mud
+	ct_run -suite wechat_mud_SUITE -logdir ./logs -pa ./_build/default/lib/proper/src
+	#@cd ../../../..
--- test/wechat_mud_SUITE.erl
+++ test/wechat_mud_SUITE.erl
@@ -10,8 +10,22 @@
 -author("shuieryin").
 
 %% API
--export([]).
+-export([
+    all/0,
+    init_per_suite/1,
+    end_per_suite/1,
+    init_per_group/2,
+    end_per_group/2,
+    init_per_testcase/2,
+    end_per_testcase/2,
+    groups/0,
+    suite/0
+]).
 
+-export([test1/1]).
+
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
 -include_lib("common_test/include/ct.hrl").
 
 %%%===================================================================
@@ -24,6 +38,41 @@
 %%
 %% @end
 %%--------------------------------------------------------------------
+suite() ->
+    [].
+
+all() -> [{group, test_group1}].
+
+init_per_suite(Config) ->
+%%    Result = os:cmd("cd ../..; make run"),
+    ct:pal("==========================Started:~n"),
+    Config.
+
+end_per_suite(_Config) ->
+    ct:pal("==========================Stopped:~n"),
+    ok.
+
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+test1(_Config) ->
+    1 = 1.
+
+groups() ->
+    [{
+        test_group1,
+        [],
+        [test1]
+    }].
 
 %%%===================================================================
 %%% Internal functions (N/A)
