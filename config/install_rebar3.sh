f3445d960cb9ecddb263abaec4e348af120f64ce
服务端 - 郭立 <shuieryin@gmail.com>
2017-05-19 19:21:00 +0800

    added proxy

--- config/install_rebar3.sh
+++ config/install_rebar3.sh
@@ -4,6 +4,9 @@ if [ -f "./config/rebar3" ]; then
     exit
 fi
 
+export http_proxy=${LOCAL_PROXY}
+export https_proxy=${LOCAL_PROXY}
+
 git clone --depth=1 https://github.com/rebar/rebar3.git
 cd rebar3
 ./bootstrap
