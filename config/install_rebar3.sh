5366da2a8fb366e257e54716f3e4ef4dcc7a87ad
Collin <shuieryin@gmail.com>
2018-02-08 18:30:21 +0800

    update install rebar3

--- config/install_rebar3.sh
+++ config/install_rebar3.sh
@@ -4,12 +4,7 @@ if [ -f "./config/rebar3" ]; then
     exit
 fi
 
-export http_proxy=${LOCAL_PROXY}
-export https_proxy=${LOCAL_PROXY}
-
-git clone --depth=1 https://github.com/rebar/rebar3.git
-cd rebar3
-./bootstrap
+cd config
+wget https://github.com/erlang/rebar3/releases/download/3.5.0/rebar3
+chmod +x rebar3
 cd ..
-cp ./rebar3/rebar3 ./config/
-rm -rf rebar3
